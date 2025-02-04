015295f124a991d4c57fffe1dde21b240e32b353
Ayush Gupta <ayushgupta3902@gmail.com>
2025-02-04 22:03:22 +0530

    feat: connect database; setup models

---
+++ src/lib/db.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URL!;
+
+if (!MONGODB_URI) {
+  throw new Error("Please define MongoDB URI in Environment Variable.");
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const options = {
+      bufferCommands: true,
+      maxPoolSize: 10,
+    };
+
+    cached.promise = mongoose
+      .connect(MONGODB_URI, options)
+      .then(() => mongoose.connection);
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (error: any) {
+    cached.promise = null;
+    throw new Error("Error while connecting with Database: ", error);
+  }
+
+  return cached.conn;
+}
---
+++ src/models/video.model.ts
@@ -0,0 +1,37 @@
+import { IVideo, VIDEO_DIMENSIONS } from "@/types/video.types";
+import { model, models, Schema } from "mongoose";
+
+const videoSchema = new Schema<IVideo>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    videoUrl: {
+      type: String,
+      required: true,
+    },
+    thumbnailUrl: {
+      type: String,
+      required: true,
+    },
+    controls: {
+      type: Boolean,
+      default: true,
+    },
+    transformation: {
+      height: { type: Number, default: VIDEO_DIMENSIONS.height },
+      width: { type: Number, default: VIDEO_DIMENSIONS.width },
+      quality: { type: Number, min: 1, max: 100 },
+    },
+  },
+  { timestamps: true }
+);
+
+const Video = models?.Video || model<IVideo>("Video", videoSchema);
+
+export default Video;
